66e6e4e79b6d50d87a37d624664298de7f12ed5a
ahmedmada <ahmedmada1996@gmail.com>
2018-03-13 17:51:33 +0200

    Update Albums.java

--- app/src/main/java/com/basic/nanodegree/musicalstructureapp/Albums.java
+++ app/src/main/java/com/basic/nanodegree/musicalstructureapp/Albums.java
@@ -2,6 +2,8 @@ package com.basic.nanodegree.musicalstructureapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Albums extends AppCompatActivity {
 
@@ -10,8 +12,10 @@ public class Albums extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
     }
+
     public void albumClick(View view) {
         Toast.makeText(Albums.this,"Album Click",Toast.LENGTH_LONG).show();
     }
+
 
 }
