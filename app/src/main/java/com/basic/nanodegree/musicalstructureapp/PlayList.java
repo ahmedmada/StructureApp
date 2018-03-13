935256c80c1e744c000db19491ffa0cdffe2d2b0
ahmedmada <ahmedmada1996@gmail.com>
2018-03-13 17:53:11 +0200

    Update PlayList.java

--- app/src/main/java/com/basic/nanodegree/musicalstructureapp/PlayList.java
+++ app/src/main/java/com/basic/nanodegree/musicalstructureapp/PlayList.java
@@ -2,12 +2,30 @@ package com.basic.nanodegree.musicalstructureapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class PlayList extends AppCompatActivity {
+public class Player extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_list);
+        setContentView(R.layout.activity_player);
     }
+    public void forward(View view) {
+        Toast.makeText(Player.this,"forward",Toast.LENGTH_LONG).show();
+    }
+    public void stop(View view) {
+        Toast.makeText(Player.this,"stop",Toast.LENGTH_LONG).show();
+    }
+    public void play(View view) {
+        Toast.makeText(Player.this,"play",Toast.LENGTH_LONG).show();
+    }
+    public void pause(View view) {
+        Toast.makeText(Player.this,"pause",Toast.LENGTH_LONG).show();
+    }
+    public void rewind(View view) {
+        Toast.makeText(Player.this,"rewind",Toast.LENGTH_LONG).show();
+    }
+
 }
