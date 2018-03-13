debebd0787716793b38b4cb84ec168cfa06e8f6e
ahmedmada <ahmedmada1996@gmail.com>
2018-03-13 17:53:33 +0200

    Update Player.java

--- app/src/main/java/com/basic/nanodegree/musicalstructureapp/Player.java
+++ app/src/main/java/com/basic/nanodegree/musicalstructureapp/Player.java
@@ -2,12 +2,17 @@ package com.basic.nanodegree.musicalstructureapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class Player extends AppCompatActivity {
+public class PlayList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_play_list);
+    }
+    public void playListClick(View view) {
+        Toast.makeText(PlayList.this,"PlayList Click",Toast.LENGTH_LONG).show();
     }
 }
