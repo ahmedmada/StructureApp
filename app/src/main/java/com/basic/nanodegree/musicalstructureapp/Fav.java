261414d99b3632106b2569ddf4bf337711bb382c
ahmedmada <ahmedmada1996@gmail.com>
2018-03-13 17:52:28 +0200

    Update Fav.java

--- app/src/main/java/com/basic/nanodegree/musicalstructureapp/Fav.java
+++ app/src/main/java/com/basic/nanodegree/musicalstructureapp/Fav.java
@@ -2,6 +2,8 @@ package com.basic.nanodegree.musicalstructureapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Fav extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Fav extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav);
+    }
+    public void favSong(View view) {
+        Toast.makeText(Fav.this,"Fav Song",Toast.LENGTH_LONG).show();
     }
 }
